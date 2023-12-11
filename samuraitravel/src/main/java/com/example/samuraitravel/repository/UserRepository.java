2c3fcc407678e753a4a8ffb31ae0c4a300e2b84e
滑川卓弥 <tatatatatata89@gmail.com>
2023-12-11 22:44:47 +0900

    adminUsershow

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/UserRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.samuraitravel.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.samuraitravel.entity.User;
 //メールアドレス検索用メソッド
 public interface UserRepository extends JpaRepository<User, Integer>{
 	public User findByEmail(String email);
+	//名前とフリガナ検索用
+	public Page<User> findByNameLikeOrFuriganaLike(String nameKeyword, String furiganaKeyword, Pageable pageble);
 }
